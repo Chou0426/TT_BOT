dd55b00bf0ffe18b92246d143d26bbef7c495b42
Chou0426 <58468867+Chou0426@users.noreply.github.com>
2020-08-10 01:23:02 +0800

    v1.2

--- cmds/myslf.py
+++ cmds/myslf.py
@@ -53,7 +53,7 @@ class Myslf(Cog_Extention):
                             Link=urltitle.find("a")
                             await ctx.send(str(count+1)+'.'+str(Link.string))
                             await ctx.send('https://www.ptt.cc/'+Link['href'])
-                            await asyncio.sleep(0.1) #單位:秒 
+                            await asyncio.sleep(0.01) #單位:秒 
                             # webbrowser.open_new('https://www.ptt.cc/'+Link['href']) 
                     count = count + 1
             
@@ -83,46 +83,21 @@ class Myslf(Cog_Extention):
             i = i + 1
             count = 0
             
-#     @commands.command()
-#     async def pttB2(self,ctx):
-#         B2url = myUrl
-#         count = 0
-#         #抓取ptt表特版的網頁原始碼(HTML)
-#         #建立一個 Request 物件，附加 Request headers 的資訊
-#         for i in B2url:
-#             request=req.Request(i,headers={
-#                 "cookie":"over18=1",
-#                 "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36"})
-#             with req.urlopen(request) as response:
-#                 data=response.read().decode("utf-8")
-#             #解析原始碼，取得每篇文章的標題
-#             root=bs4.BeautifulSoup(data,"html.parser") #讓BeautifulSoup協助我們解析HTML格式文件
-#             titles=root.find_all("a") 
-#             urltitles = root.find_all("div",class_="title")
-            
-#             for urltitle in urltitles:
-#                 if urltitle.a != None:
-#                     if count < 5:
-#                         Link=urltitle.find("a")
-#                         await ctx.send(str(count+1)+'.'+str(Link.string))
-#                         await ctx.send('https://www.ptt.cc/'+Link['href'])
-#                         await asyncio.sleep(0.1) #單位:秒 
-#                         # webbrowser.open_new('https://www.ptt.cc/'+Link['href'])
-                        
-#                     # elif count == 5 :
-#                     #     nextLinkTag=root.find("a",string="‹ 上頁")
-#                     #     nextLink2 = 'https://www.ptt.cc/'+nextLinkTag['href']
-#                     #     await ctx.send("下一頁的網址:" + nextLink2)
-#                     #     nextLink22 = nextLink2
-                        
-                        
-#                         # nextLinkCon=urltitle.find("a")
-#                         # await ctx.send("下一頁的內容:")
-#                         # await ctx.send(str(nextLinkCon.string))
-#                         # await ctx.send('https://www.ptt.cc/'+nextLinkCon['href'])
-#                         # await asyncio.sleep(0.1) #單位:秒 
-#                         # # webbrowser.open_new('https://www.ptt.cc/'+nextLinkCon['href'])
-#                     count = count + 1
-        
+    @commands.command()
+    async def pttB2(self,ctx,picUrl):
+        request=req.Request(picUrl,headers={
+                "cookie":"over18=1",
+                "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36"})
+        with req.urlopen(request) as response:
+            data=response.read().decode("utf-8")
+            #解析原始碼，取得每篇文章的標題
+        root=bs4.BeautifulSoup(data,"html.parser") #讓BeautifulSoup協助我們解析HTML格式文件
+        titles=root.find_all("a") 
+        for title in titles:
+            if str(title.string).endswith("jpg"):
+                await asyncio.sleep(0.1) #單位:秒
+                await ctx.send(title["href"])
+                # await ctx.send(str(title.string))
+    
 def setup(bot):
     bot.add_cog(Myslf(bot))
